0fc46b9d10bc7d5850bfb0c960ff671e793cc613
Lorenzo Ruiz <lars.bs@hotmail.com>
2018-09-06 13:03:43 +0200

    Add missing babel plugins

--- .babelrc.js
+++ .babelrc.js
@@ -8,5 +8,8 @@ module.exports = {
   "plugins": [
     "@larsbs/babel-plugin-react-docgen",
     "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-do-expressions",
   ],
 };
