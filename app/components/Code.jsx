ab4be9ff86f33f56ade6df57b74e6a06ada092e6
Lorenzo Ruiz <lars.bs@hotmail.com>
2018-10-02 17:10:24 +0200

    Improve code

--- app/components/Code.jsx
+++ app/components/Code.jsx
@@ -17,10 +17,19 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     '& .CodeMirror': {
       fontFamily: '"Fira Code", "monaco", monospaced',
-      padding: '8px',
       height: 'auto',
       fontSize: '14px !important',
       lineHeight: '1.8',
+    },
+    '& .CodeMirror-linenumber': {
+      paddingLeft: 8,
+      paddingRight: 20,
+    },
+    '& .CodeMirror-gutters': {
+      background: 'rgba(0, 0, 0, 0.4) !important',
+    },
+    '& .CodeMirror-lines': {
+      marginLeft: 8,
     },
   },
 });
@@ -44,7 +53,7 @@ class Code extends React.Component {
   componentDidMount() {
     const { children, className } = this.props;
     this.code = CodeMirror.fromTextArea(this.textarea, {
-      lineNumbers: false,
+      lineNumbers: this._shouldLineNumbers(),
       mode: getMode(className),
       theme: 'material',
       readOnly: 'nocursor',
@@ -58,6 +67,11 @@ class Code extends React.Component {
         <textarea ref={(ref) => this.textarea = ref} />
       </div>
     );
+  }
+
+  _shouldLineNumbers() {
+    const { children } = this.props;
+    return children.split('\n').length > 2;
   }
 
 }
